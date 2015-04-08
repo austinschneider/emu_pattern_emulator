214b99684a3d8e134463303eeaafabac8b2713bf
Austin Schneider <hogenshpogen@gmail.com>
2015-04-08 15:25:28 -0500

    Added command constants.

--- Emulator_constants.h
+++ Emulator_constants.h
@@ -13,6 +13,12 @@ namespace pc {
 
 static const unsigned long int RAMPAGE_SIZE = 4 * 1024; // size of RAM pages from FPGA
 
+static const unsigned long int status_cmd           = 0x0000F0;
+static const unsigned long int f1_cmd               = 0x0000F1;
+static const unsigned long int f2_cmd               = 0x0000F2;
+static const unsigned long int read_cmd             = 0x0000F3;
+static const unsigned long int write_cmd            = 0x0000F7;
+
 } // namespace pc
 } // namespace emu
 
